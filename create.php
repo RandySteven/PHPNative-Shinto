9c5f2af0a19fbc1e806eb13fdac519377a46060e
unknown <randysteven12@gmail.com>
2021-05-21 16:31:09 +0700

    Shinto

---
+++ create.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan Script' rel='stylesheet'>
+    <style>
+      html, body {
+        margin: 0px;
+        font-family: 'Kaushan Script';font-size: 22px;
+      }
+      .jumbotron{
+        background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("assets/images/golden-pavilion-kinkakuji-temple-kyoto-japan\ 1.png");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 2rem;
+      }
+      .text-red{
+        color: red;
+      }
+      .h1{
+        font-size: 120px;
+      }
+    </style>
+<title>Shrines</title>
+</head>
+<body>
+    <div class="jumbotron">
+
+      <nav class="navbar navbar-expand-lg mt-0">
+        <div class="container-fluid">
+          <a class="navbar-brand text-red" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link text-red" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="create.html">Create</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link text-red dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <h1 class="text-center py-xl-5 text-red h1">Shrines</h1>
+    </div>
+
+    <div class="container mt-5">
+
+        <form>
+            <div class="mb-3">
+              <label for="shrine_name" class="form-label">Shrine Name</label>
+              <input type="text" class="form-control" id="shrine_name" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="shrine_name_japan" class="form-label">Shrine Name Japan</label>
+              <input type="text" class="form-control" id="shrine_name_japan">
+            </div>
+            <div class="mb-3">
+                <label for="islands" class="form-label">Island</label>
+                <select name="islands" id="" class="form-control">
+                    <?php 
+                      $islands = array("Hokkaido", "Touhoku", "Kantou", "Chuubu", "Kansai", "Chuugoku", "Shikoku", "Kyuushu"); 
+                      
+                      for($i = 0 ; $i < count($islands) ; $i++){
+                    ?>
+                      <option value="<?php echo $islands[$i]?>"><?php echo $islands[$i]?></option>
+                    <?php } ?>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan Script' rel='stylesheet'>
+    <style>
+      html, body {
+        margin: 0px;
+        font-family: 'Kaushan Script';font-size: 22px;
+      }
+      .jumbotron{
+        background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("assets/images/golden-pavilion-kinkakuji-temple-kyoto-japan\ 1.png");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 2rem;
+      }
+      .text-red{
+        color: red;
+      }
+      .h1{
+        font-size: 120px;
+      }
+    </style>
+<title>Shrines</title>
+</head>
+<body>
+    <div class="jumbotron">
+
+      <nav class="navbar navbar-expand-lg mt-0">
+        <div class="container-fluid">
+          <a class="navbar-brand text-red" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-red" href="create.html">Create</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link text-red dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <h1 class="text-center py-xl-5 text-red h1">Shrines</h1>
+    </div>
+
+    <div class="container mt-5">
+      <div class="card mb-3 w-auto border-2 border-dark">
+        <div class="row g-0">
+          <div class="col-md-2">
+            <img src="assets/images/220px-Hokkaido_Jingu 1.png" class="mx-2 my-5 rounded-1" width="200" height="179" alt="...">
+          </div>
+          <div class="col-md-10">
+            <div class="card-body">
+              <h3 class="card-title">Hokkaido Jingu, 北海道神宮</h3>
+              <p class="card-text"><small class="text-muted">Hokkaido</small>, <small class="text-muted">Hokkaido</small></p>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 w-auto border-2 border-dark">
+        <div class="row g-0">
+          <div class="col-md-2">
+            <img src="assets/images/220px-Hokkaido_Jingu 1.png" class="mx-2 my-5 rounded-1" width="200" height="179" alt="...">
+          </div>
+          <div class="col-md-10">
+            <div class="card-body">
+              <h3 class="card-title">Hokkaido Jingu, 北海道神宮</h3>
+              <p class="card-text"><small class="text-muted">Hokkaido</small>, <small class="text-muted">Hokkaido</small></p>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>   
+</body>
+</html>
